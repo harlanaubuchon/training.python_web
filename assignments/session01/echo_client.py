908d344e3d4255be0538b580bcad9fb6b6a772cc
unknown <chq-harlana@CHQ-HARLANA.expd>
2014-01-14 17:09:35 -0800

    Mostly finished homework.  Still having issues with reusing socket connections.

--- assignments/session01/echo_client.py
+++ assignments/session01/echo_client.py
@@ -6,16 +6,19 @@ def client(msg, log_buffer=sys.stderr):
     server_address = ('localhost', 10000)
     # TODO: Replace the following line with your code which will instantiate 
     #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = None
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
     print >>log_buffer, 'connecting to {0} port {1}'.format(*server_address)
     # TODO: connect your socket to the server here.
+    sock.connect(server_address)
 
     # this try/finally block exists purely to allow us to close the socket
     # when we are finished with it
     try:
         print >>log_buffer, 'sending "{0}"'.format(msg)
         # TODO: send your message to the server here.
-
+        sock.sendall(msg)
         # TODO: the server should be sending you back your message as a series
         #       of 16-byte chunks.  You will want to log them as you receive 
         #       each one.  You will also need to check to make sure that 
@@ -24,12 +27,22 @@ def client(msg, log_buffer=sys.stderr):
         # 
         #       Make sure that you log each chunk you receive.  Use the print 
         #       statement below to do it. (The tests expect this log format)
-        chunk = ''
-        print >>log_buffer, 'received "{0}"'.format(chunk)
+        data = ''
+        buffsize = 16
+        done = False
+        while not done:
+            chunk = sock.recv(buffsize)
+            print >>log_buffer, 'received "{0}"'.format(chunk)
+            if len(chunk) < buffsize:
+                done = True
+            data += chunk
+
+
     finally:
         # TODO: after you break out of the loop receiving echoed chunks from 
         #       the server you will want to close your client socket.
         print >>log_buffer, 'closing socket'
+        sock.close()
 
 
 if __name__ == '__main__':
