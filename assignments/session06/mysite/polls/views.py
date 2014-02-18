9e295865070dec1376a47d49b2a5801af059b1ed
harlanaubuchon <harlanaubuchon@gmail.com>
2014-02-17 23:57:37 -0800

    Finished tutorial

--- assignments/session06/mysite/polls/views.py
+++ assignments/session06/mysite/polls/views.py
@@ -4,11 +4,26 @@ from polls.models import Poll, Choice
 from django.template import RequestContext, loader
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
 
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    context = {'latest_poll_list': latest_poll_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published polls (not including those set to be
+        published in the future).
+        """
+        return Poll.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+#def index(request):
+#    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+#    context = {'latest_poll_list': latest_poll_list}
+#    return render(request, 'polls/index.html', context)
 
 #def index(request):
 #    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
@@ -21,26 +36,43 @@ def index(request):
 #def index(request):
 #    return HttpResponse("Hello, world. You are at the poll index")
 
-def detail(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    #try:
-    #    poll = Poll.objects.get(pk=poll_id)
 
-    #except Poll.DoesNotExist:
-    #    raise Http404
-    return render(request, "polls/detail.html", {'poll': poll})
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any polls that aren't published yet.
+        """
+        return Poll.objects.filter(pub_date__lte=timezone.now())
+
+#class DetailView(generic.DetailView):
+#    model = Poll
+#    template_name = 'polls/detail.html'
+
+#def detail(request, poll_id):
+#    poll = get_object_or_404(Poll, pk=poll_id)
+#    #try:
+#    #    poll = Poll.objects.get(pk=poll_id)
+#
+#    #except Poll.DoesNotExist:
+#    #    raise Http404
+#    return render(request, "polls/detail.html", {'poll': poll})
 
 
 #def results(request, poll_id):
 #    return HttpResponse("You're looking at the results of poll %s." % poll_id)
 
-def results(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/results.html', {'poll': poll})
+#def results(request, poll_id):
+#    poll = get_object_or_404(Poll, pk=poll_id)
+#    return render(request, 'polls/results.html', {'poll': poll})
+#
 
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
 
-#def vote(request, poll_id):
-#    return HttpResponse("You're voting on poll %s." % poll_id)
 
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
@@ -59,4 +91,7 @@ def vote(request, poll_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+##def vote(request, poll_id):
+##    return HttpResponse("You're voting on poll %s." % poll_id)
 
