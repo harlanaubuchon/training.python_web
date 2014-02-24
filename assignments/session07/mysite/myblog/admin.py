3298959dc139c2219cfe438c1e8b6a1a825f6d6c
harlanaubuchon <harlanaubuchon@gmail.com>
2014-02-23 17:56:40 -0800

    Finished required tasks 1-4 (Note: Updated admin user:password to admin:admin)

--- assignments/session07/mysite/myblog/admin.py
+++ assignments/session07/mysite/myblog/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 from myblog.models import Post
 from myblog.models import Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoryInline(admin.StackedInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ('title', 'text', 'author', 'published_date',)
+    inlines = [
+        CategoryInline,
+    ]
+admin.site.register(Post, PostAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+admin.site.register(Category, CategoryAdmin)
+
+
--- assignments/session07/mysite/myblog/models.py
+++ assignments/session07/mysite/myblog/models.py
@@ -2,15 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-	title = models.CharField(max_length=128)
-	text = models.TextField(blank=True)
-	author = models.ForeignKey(User)
-	created_date = models.DateTimeField(auto_now_add=True)
-	modified_date = models.DateTimeField(auto_now=True)
-	published_date = models.DateTimeField(blank=True, null=True)
+    title = models.CharField(max_length=128)
+    text = models.TextField(blank=True)
+    author = models.ForeignKey(User)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
-	def __unicode__(self):
-		return self.title
+    def __unicode__(self):
+        return self.title
 
 class Category(models.Model):
     name = models.CharField(max_length=128)
@@ -20,3 +20,4 @@ class Category(models.Model):
 
     def __unicode__(self):
         return self.name
+
